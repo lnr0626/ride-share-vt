92c4a6103acce7e14de1483a57f3e29e61c87fcc
Lloyd Ramey <lnr0626@gmail.com>
2013-12-11 22:51:28 -0500

    Removed generated files

--- RideShareVT/build/source/buildConfig/debug/ece4564/ridesharevt/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ece4564.ridesharevt;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
